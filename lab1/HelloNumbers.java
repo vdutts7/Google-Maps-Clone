18f71c8b090c3cb685cdca21f0e374b5d48c8346
Vivek Dutta <vivekdutta2000@hotmail.com>
2020-09-04 04:21:23 -0700

    submission pt3 for lab1, vivekdutta, s402

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,9 +1,11 @@
 public class HelloNumbers {
 	public static void main(String[] args) {
 		int x = 0;
+		int sum = x;
 		while (x < 10) {
-			System.out.println(x + " ");
+			System.out.println(sum + " ");
 			x++;
+			sum += x;
 		}
 
 	}
